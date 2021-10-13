4624c35d1250ef57c74bff8f19f018e593f17bb0
Kevin Duenas <kevinduenas@hotmail.es>
2021-10-13 10:01:39 -0500

    Initial Commit

---
+++ PaControl/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaControl
+//
+//  Created by Kevin Duenas on 13/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
